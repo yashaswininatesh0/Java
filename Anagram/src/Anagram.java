21584c25879ed77432d8ac5c90bba549f5075ce6
yashaswiniachar <user@DESKTOP-T797UCS>
2022-01-26 16:49:36 +0530

    first commit

---
+++ Anagram/src/Anagram.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Anagram {
+  public static void main(String[] args) {
+	  Scanner sc=new Scanner(System.in);
+		String str1=sc.next();
+		String str2=sc.next();
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+
+    if(str1.length() == str2.length()) {
+
+      char[] charArray1 = str1.toCharArray();
+      char[] charArray2 = str2.toCharArray();
+
+      Arrays.sort(charArray1);
+      Arrays.sort(charArray2);
+
+      boolean result = Arrays.equals(charArray1, charArray2);
+
+      if(result) {
+        System.out.println(str1 + " and " + str2 + " are anagram.");
+      }
+      else {
+        System.out.println(str1 + " and " + str2 + " are not anagram.");
+      }
+    }
+    else {
+      System.out.println(str1 + " and " + str2 + " are not anagram.");
+    }
+  }
+}
